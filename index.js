5b79f47a28bede52dbe794d08351534b90a3ee78
Hri7566 <hri7566@gmail.com>
2022-09-22 21:14:00 -0400

    commands

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const channel = '#dev-room'
 let cl = new Client('wss://mppclone.com:8443', process.env.MPPCLONE_TOKEN);
 let irc = new ircClient('raspberrypi', 6697, nick, nick);
 
+let prefix = '!';
+
 cl.start();
 cl.setChannel('✧𝓓𝓔𝓥 𝓡𝓸𝓸𝓶✧')
 
@@ -29,8 +31,28 @@ const sendChat = str => {
     }]);
 }
 
+const ircChat = str => {
+    irc.say(channel, str);
+}
+
+let ping = -1;
+
+setInterval(() => {
+    let ms = Date.now();
+    cl.sendArray([{
+        m: 't',
+        e: ms
+    }]);
+}, 5000);
+
+cl.on('t', msg => {
+    ping = msg.t - msg.e;
+});
+
 irc.client.on('data', msg => {
     msg = msg.toString();
+
+    // console.log(msg);
 
     let args = msg.split(' ');
     let type = args[1];
@@ -39,28 +61,60 @@ irc.client.on('data', msg => {
         irc.join(channel);
     }
 
+    if (type == 'QUIT') {
+        let n = args[0].substring(1, args[0].indexOf('!'))
+        sendChat(`[IRC] ${n} left the chat room`);
+    }
+
+    if (type == 'JOIN') {
+        let n = args[0].substring(1, args[0].indexOf('!'))
+        sendChat(`[IRC] ${n} joined the chat room`);
+    }
+
     if (type == 'PRIVMSG') {
         let n = args[0].substring(1, args[0].indexOf('!'))
         let str = msg.substring(args[0].length + args[1].length + args[2].length + '   '.length + ':'.length).split('\n').join(' ').split('\r').join(' ');
         
         if (nick == n) return;
 
-        sendChat(`[IRC] ${n}: ${str}`);
+        let isCommand = str.startsWith(prefix);
+
+        if (isCommand) {
+            msg = {
+                a: str,
+                p: {
+                    name: n
+                }
+            }
+
+            msg.args = msg.a.split(' ');
+            msg.argcat = msg.a.substring(msg.args[0].length);
+
+            msg.cmd = msg.args[0].split(prefix).join('');
+
+            switch (msg.cmd) {
+                case 'ping':
+                    ircChat(`Ping: ${ping}`);
+                    break;
+            }
+        } else {
+            sendChat(`[IRC] ${n}: ${str}`);
+        }
     }
 });
 
 cl.on('a', msg => {
     if (irc.client.closed) return;
     if (msg.p._id == cl.getOwnParticipant()._id && msg.a.startsWith('\u034f[IRC]')) return;
-    irc.say(channel, `[${msg.p._id.substring(0, 6)}] ${msg.p.name}: ${msg.a}`);
+    ircChat(`[${msg.p._id.substring(0, 6)}] ${msg.p.name}: ${msg.a}`);
 });
 
 cl.on('participant added', p => {
     if (irc.client.closed) return;
-    irc.say(channel, `[${p._id.substring(0, 6)}] ${p.name} joined the channel`);
+    ircChat(`[${p._id.substring(0, 6)}] ${p.name} joined the channel`);
 });
 
 cl.on('participant removed', p => {
     if (irc.client.closed) return;
-    irc.say(channel, `[${p._id.substring(0, 6)}] ${p.name} left the channel`);
+    ircChat(`[${p._id.substring(0, 6)}] ${p.name} left the channel`);
 });
